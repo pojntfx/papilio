6a5e8fea015e11953da14373a71545daf23ba40e
Felix Pojtinger <felix@pojtinger.com>
2022-02-10 21:34:44 +0100

    feat: Add input validation and proper low/high byte parsing

--- pkg/generators/fe11s/generate.go
+++ pkg/generators/fe11s/generate.go
@@ -16,24 +16,21 @@ package fe11s
 // | `0x1F`      | Check Sum             | The 8-bit sum of all value from `0x00` to `0x1E`.                                                                |
 
 func GenerateEEPROM(
-	vendorID [2]byte, // i.e. 2109 -> []byte{0x1a, 0x40} for VIA Labs, Inc.
-	productID [2]byte, // i.e. 0817 -> []byte{0x01, 0x01} for USB3.0 Hub
-	deviceReleaseNumber [2]byte, // i.e. 3.83 → []byte{0x3, 0x83}
-	numberOfDownstreamPorts byte, // i.e. 4
+	idVendor uint16, // i.e. 0x046d for Logitech
+	idProduct uint16, // i.e. 0x082d for HD Pro Webcam C920
+	bcdDevice uint16, // i.e. 0x0001 for release 1
+	numberOfDownstreamPorts uint8, // i.e. 4 for 4 ports
 ) ([]byte, error) {
 	buf := make([]byte, 0x1F) // Filling is `0x00`
 
 	buf[0x00] = 0x40 // Constant, low byte of check code
 	buf[0x01] = 0x1A // Constant, high byte of check code
 
-	buf[0x02] = vendorID[0] // Low byte of Vendor ID, `idVendor` field of Standard Device Descriptor
-	buf[0x03] = vendorID[1] // High byte of Vendor ID
+	buf[0x02], buf[0x03] = getLowAndHighByte(idVendor) // Low and high byte of Vendor ID, `idVendor` field of Standard Device Descriptor
 
-	buf[0x04] = productID[0] // Low byte of Product ID, `idProduct` field of Standard Device Descriptor
-	buf[0x05] = productID[1] // High Byte of Product ID
+	buf[0x04], buf[0x05] = getLowAndHighByte(idProduct) // Low and high byte of Product ID, `idProduct` field of Standard Device Descriptor
 
-	buf[0x06] = deviceReleaseNumber[0] // Low byte of Device Release Number, must be Binary Coded Decimal, `bcdDevice` field of Standard Device Descriptor
-	buf[0x07] = deviceReleaseNumber[1] // High byte of Device Release Number, must be Binary Coded Decimal
+	buf[0x06], buf[0x07] = getLowAndHighByte(getBCD(bcdDevice)) // Low and high byte of Device Release Number, must be Binary Coded Decimal, `bcdDevice` field of Standard Device Descriptor
 
 	buf[0x1F] = numberOfDownstreamPorts // The 8-bit sum of all value from `0x00` to `0x1E`.
 	for _, el := range buf[:0x1E] {
@@ -41,4 +38,14 @@ func GenerateEEPROM(
 	}
 
 	return buf, nil
+}
+
+func getLowAndHighByte(word uint16) (byte, byte) {
+	// See https://www.mikrocontroller.net/topic/231566
+	return uint8(word & 0xff), uint8(word >> 8)
+}
+
+func getBCD(word uint16) uint16 {
+	// See https://github.com/masayukioguni/bcd/blob/master/bcd.go#L4
+	return (((word / 10) % 10) << 4) | (word % 10)
 }
